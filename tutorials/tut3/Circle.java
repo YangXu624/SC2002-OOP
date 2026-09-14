cf05261872fa7541ad8754cf5bf3922904b181bd
YangXu624 <yangxu624@gmail.com>
2026-09-14 10:20:03 +0800

    implemented tut3 circle class

---
+++ tutorials/tut3/Circle.java
@@ -0,0 +1,21 @@
+public class Circle extends Point {
+    private double radius;
+    private static final double PI = 3.14159;
+
+    public Circle(int x, int y, double rad) {
+        super(x, y);
+        this.radius = rad;
+    }
+
+    public void setRadius(double rad) {
+        this.radius = rad;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double area() {
+        return PI * this.radius * this.radius;
+    }
+}
